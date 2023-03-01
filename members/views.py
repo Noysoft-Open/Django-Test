2a977efcfb6a6047b372771c629f4abb2cefb5b1
Xenon Zenon <j5yson5b5d@gmail.com>
2023-03-01 11:09:00 +0800

    update 0.0.3

--- members/views.py
+++ members/views.py
@@ -6,5 +6,15 @@ from .models import Member
 def members(request):
     members = Member.objects.all().values()
     template = loader.get_template('all-members.html')
-    context = {'members':members,}
+    context = {
+        'members':members,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    member = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'member':member
+    }
     return HttpResponse(template.render(context, request))
--- test/urls.py
+++ test/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landingpage.urls')),
-    path('members/', include('members.urls')),
+    path('', include('members.urls')),
 ]

950f21615e25162fe671da80bbba8898434daf58
Xenon Zenon <j5yson5b5d@gmail.com>
2023-03-02 04:33:16 +0800

    Update 0.0.5

--- members/views.py
+++ members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+from django.db.models import Q
 
 def members(request):
     members = Member.objects.all().values()
@@ -23,11 +24,9 @@ def main(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
 
-
 def testing(request):
-    members = Member.objects.all().values()
+    mydata = Member.objects.all().order_by('lastname', '-id').values()
     template = loader.get_template('template.html')
-    context = {
-        'members': members
-    }
+    context = { 'members' : mydata, }
+
     return HttpResponse(template.render(context, request))
--- test/settings.py
+++ test/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q^-khj(y_w(8f^7i5l070y3eeacq(_7hh%xh+5^ig1rwfw)h)h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ '*' ]
+ALLOWED_HOSTS = [ '127.0.0.1', 'localhost' ]
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'test.urls'
@@ -116,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'productionfiles'
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'teststaticfiles'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
